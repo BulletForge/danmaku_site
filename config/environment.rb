bf3c2c5f0d5ab24a7081152a4aef8002084f990c
Aaron Qian <aq1018@gmail.com>
2010-01-16 21:17:36 +0800

    remove resource_controller and adding inherited_resources gem

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "authlogic"
   config.gem "searchlogic"
   config.gem "will_paginate"
+  config.gem "inherited_resources"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

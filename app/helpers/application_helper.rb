cb3161eb0ff3dfe26ea49d63fc3137010a6f4fa9
Blargel <LargeBagel@gmail.com>
2010-02-25 15:24:02 +0800

    Added a featured project to the home page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def preview_image(project)
+  def preview_image(project, type)
     image = image_tag 'nopreview.png'
-    image = image_tag project.images.first.attachment.url(:thumb) unless project.images.empty?
+    image = image_tag project.images.first.attachment.url(type) unless project.images.empty?
     link_to image, user_project_path(project.user, project)
   end
 end

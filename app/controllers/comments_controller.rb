ed969c4036c2fa91f5138882c6330cb6d97b1ea2
Aaron Qian <aq1018@gmail.com>
2010-03-03 10:56:25 +0800

    fixing comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,69 +1,60 @@
 class CommentsController < ApplicationController
-
+  before_filter :comments, :only => :index
+  before_filter :comment, :only =>  :destroy
+  before_filter :build_comment, :only => :create
+  
   def index
-    if params[:user_id]
-      @user = User.find_by_permalink params[:user_id]
-      if params[:project_id]
-        @project = @user.projects.find_by_permalink params[:project_id]
-        if params[:version_id]
-          @version = @project.versions.find_by_permalink params[:version_id]
-          @comments = @version.comments
-        else
-          @comments = @project.comments
-        end
-      else
-        @comments = @user.comments
-      end
-    end
+    @comments = comments
   end
 
   def create
-    if params[:user_id]
-      @user = User.find_by_permalink params[:user_id]
-      if params[:project_id]
-        @project = @user.projects.find_by_permalink params[:project_id]
-        if params[:version_id]
-          @version = @project.versions.find_by_permalink params[:version_id]
-        end
-      end
-    end
-
-    @comment = Comment.new(params[:comment])
+    @comment = build_comment
     if @comment.save
       flash[:notice] = "Comment successfully posted."
     else
       flash[:notice] = "Comment posting failed."
     end
-
-    if @version
-      redirect_to user_project_version_path(@user, @project, @version)
-    elsif @project
-      redirect_to user_project_path(@user, @project)
-    elsif @user
-      redirect_to user_path(@user)
-    else
-      redirect_to root_path
-    end
+    redirect_back    
   end
 
   def destroy
+    @comment = comment
+    @comment.destroy
+    redirect_back
+  end
+  
+  private
+  
+  def comments
+    @comments = end_of_association_chain
+  end
+  
+  def build_comment
+    @comment = end_of_association_chain.find(params[:comment])
+  end
+  
+  def comment
+    @comment = end_of_association_chain.find(params[:id])
+  end
+  
+  def end_of_association_chain
+    @end_of_association_chain ||= ( parent.nil? ? Comment : parent.comments )
+  end
+  
+  def parent
     if params[:user_id]
-      @user = User.find_by_permalink params[:user_id]
+      @user = User.find_by_permalink!(params[:user_id])
       if params[:project_id]
-        @project = @user.projects.find_by_permalink params[:project_id]
+        @project = @user.projects.find_by_permalink!(params[:project_id])
         if params[:version_id]
-          @version = @project.versions.find_by_permalink params[:version_id]
-          @comment = @version.comments.find params[:id]
-        else
-          @comment = @project.comments.find params[:id]
+          @version = @project.versions.find_by_permalink!(params[:version_id])
         end
-      else
-        @comment = @user.comments.find params[:id]
       end
     end
-
-    @comment.destroy
-
+    @parent ||= (@version || @project || @user)
+  end
+  
+  def redirect_back
     if @version
       redirect_to user_project_version_path(@user, @project, @version)
     elsif @project

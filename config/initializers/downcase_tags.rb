496f38a180a723657d1d2fdf6ccdaab30dba0242
Aaron Qian <aaron@ekohe.com>
2010-03-17 09:43:00 +0800

    downcase all tags

---
+++ config/initializers/downcase_tags.rb
@@ -0,0 +1,7 @@
+TagList.class_eval do
+  def extract_and_apply_options_with_downcase!(args)
+    extract_and_apply_options_without_downcase!(args).map!(&:downcase)
+  end
+  
+  alias_method_chain :extract_and_apply_options!, :downcase
+end

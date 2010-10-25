98a7345c3bd0f329eb709e14cf02741f637121dc
Abner Qian <LargeBagel@gmail.com>
2010-10-25 12:41:29 -0700

    Fixed cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -42,22 +42,22 @@ class Ability
       end      
       
       # manage his own projects
-      can :manage, Project do |action, project|
+      can :manage, Project do |project|
         project.user == user
       end
 
       # manage his own image if he can manage the project
-      can :manage, Image do |action, image|
+      can :manage, Image do |image|
         image.project && can?(:manage, image.project)
       end
       
       # manage versions
-      can :manage, Version do |action, version|
+      can :manage, Version do |version|
         version.project && can?(:manage, version.project)
       end
       
       # manage archives
-      can :manage, Archive do |action, archive|
+      can :manage, Archive do |archive|
         archive.version && can?(:manage, archive.version)
       end
       

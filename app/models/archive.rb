6f43b137c52903140c2e18d0e3cb93b2caa8ba96
Abner Qian <LargeBagel@gmail.com>
2010-11-10 16:51:44 -0800

    Changed s3 parameters in paperclip

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -2,6 +2,7 @@ class Archive < Asset
   has_attached_file :attachment,
     :storage => :s3,
     :s3_credentials => '/home/deploy/s3.yml',
+    :path => ':archive/:id/:name.:extension',
     :bucket => 'bulletforge_bucket'
   MAX_FILE_SIZE = '300 MB'
 end

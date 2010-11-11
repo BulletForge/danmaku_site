004efb9a549de3a806648f4946d6a0d95048dbfe
Abner Qian <LargeBagel@gmail.com>
2010-11-10 20:55:36 -0800

    Fixed typos

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,8 +1,8 @@
 class Archive < Asset
   has_attached_file :attachment,
     :storage => :s3,
-    :s3_credentials => {:access_key_id => Bulletforge.s3_key, :secret_access_key => Bulletforge.s3_secret},
+    :s3_credentials => {:access_key_id => BulletForge.s3_key, :secret_access_key => BulletForge.s3_secret},
     :path => 'system/:id/:filename',
-    :bucket => Bulletforge.s3_bucket
+    :bucket => BulletForge.s3_bucket
   MAX_FILE_SIZE = '300 MB'
 end
--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,10 +1,10 @@
 if Rails.env.production? 
-  Bulletforge.s3_key = ENV['S3_KEY']
-  Bulletforge.s3_secret = ENV['S3_SECRET']
-  Bulletforge.s3_bucket = ENV['S3_BUCKET']
+  BulletForge.s3_key = ENV['S3_KEY']
+  BulletForge.s3_secret = ENV['S3_SECRET']
+  BulletForge.s3_bucket = ENV['S3_BUCKET']
 else
   config = YAML.load_file("#{Rails.root}/config/s3.yml")[Rails.env.to_s]
-  Bulletforge.s3_key = config['key']
-  Bulletforge.s3_secret = config['secret']
-  Bulletforge.s3_bucket = config['bucket']
+  BulletForge.s3_key = config['key']
+  BulletForge.s3_secret = config['secret']
+  BulletForge.s3_bucket = config['bucket']
 end

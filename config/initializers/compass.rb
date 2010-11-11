74476b1bfb6ada36463f7a91fac0bd56d4fa4ff0
Abner Qian <LargeBagel@gmail.com>
2010-11-10 20:09:25 -0800

    Changed gitignore and made SASS not compile automatically

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -3,3 +3,4 @@ require 'compass'
 Compass.add_project_configuration(File.join(RAILS_ROOT, "config", "compass.config"))
 Compass.configuration.environment = RAILS_ENV.to_sym
 Compass.configure_sass_plugin!
+Sass::Plugin.options[:never_update] = true

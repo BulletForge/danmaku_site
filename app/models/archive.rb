9cdec34b69708c17547110b233879e8d55e7ca11
Abner Qian <LargeBagel@gmail.com>
2010-11-10 17:28:07 -0800

    Changed path in paperclip s3 again, testing

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -2,7 +2,7 @@ class Archive < Asset
   has_attached_file :attachment,
     :storage => :s3,
     :s3_credentials => '/home/deploy/s3.yml',
-    :path => 'system/:id/:file_name.:extension',
+    :path => 'system/:id/:archive_file_name.:extension',
     :bucket => 'bulletforge_bucket'
   MAX_FILE_SIZE = '300 MB'
 end

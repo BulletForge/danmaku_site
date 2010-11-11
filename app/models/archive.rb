20359fb566711cb9a45fdefa15da37936d867df9
Abner Qian <LargeBagel@gmail.com>
2010-11-10 20:52:27 -0800

    Changed how s3 credentials and buckets are stored

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,8 +1,8 @@
 class Archive < Asset
   has_attached_file :attachment,
     :storage => :s3,
-    :s3_credentials => '/Users/abner/s3.yml',
+    :s3_credentials => {:access_key_id => Bulletforge.s3_key, :secret_access_key => Bulletforge.s3_secret},
     :path => 'system/:id/:filename',
-    :bucket => 'bulletforge_bucket'
+    :bucket => Bulletforge.s3_bucket
   MAX_FILE_SIZE = '300 MB'
 end

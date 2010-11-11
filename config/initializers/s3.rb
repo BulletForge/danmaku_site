162204e41189fad5ee11211ec2a4d904555c5eb7
Abner Qian <LargeBagel@gmail.com>
2010-11-10 21:57:08 -0800

    Monkey patching s3_swf_upload to use environment variables

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
   BulletForge.s3_secret = ENV['S3_SECRET']
   BulletForge.s3_bucket = ENV['S3_BUCKET']
 else
-  config = YAML.load_file("#{Rails.root}/config/s3.yml")[Rails.env.to_s]
-  BulletForge.s3_key = config['key']
-  BulletForge.s3_secret = config['secret']
+  config = YAML.load_file("#{Rails.root}/config/amazon_s3.yml")[Rails.env.to_s]
+  BulletForge.s3_key = config['access_key_id']
+  BulletForge.s3_secret = config['secret_access_key']
   BulletForge.s3_bucket = config['bucket']
 end

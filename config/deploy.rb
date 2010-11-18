99fe568bd03b1bbc7e0570f0460768d0d6efdde0
Abner Qian <LargeBagel@gmail.com>
2010-11-17 17:14:28 -0800

    Changing gitignore and deploy.rb to prepare for data migration to S3 and Heroku

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,9 +27,11 @@ set :deploy_to, '/var/www/bulletforge'
 after 'deploy:update_code', :roles => :app do
   run <<-BASH
     rm -f #{current_release}/config/database.yml &&
+    rm -f #{current_release}/config/amazon_s3.yml &&
     rm -rf #{current_release}/public/system &&
     rm -rf #{current_release}/public/cache &&
     ln -s #{shared_path}/config/database.yml #{current_release}/config/database.yml &&
+    ln -s #{shared_path}/config/amazon_s3.yml #{current_release}/config/amazon_s3.yml &&
     ln -s #{shared_path}/public/system #{current_release}/public/ &&
     ln -s #{shared_path}/cache #{current_release}/tmp/ &&
     cd #{current_release} && bundle install

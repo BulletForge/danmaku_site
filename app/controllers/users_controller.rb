bf1ab2b7ef98f559728e0377b909659ddf3763c1
abnerqian <abnerqian@attinteractive.com>
2012-03-23 14:08:13 -0700

    Fixing sort order for user project counts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
       @users = @users.order("#{column} #{direction}")
     elsif "projects_count" == column
       @users = @users.all.sort_by{ |u| u.projects.count }
-      @users.reverse if direction == "ASC"
+      @users = @users.reverse if direction == "ASC"
     end
   end
 end

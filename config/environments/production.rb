8f7cb8f06b68060b5a4fc11241174fcd2750f1cf
Abner Qian <LargeBagel@gmail.com>
2019-06-03 14:44:47 -0700

    Forcing https only in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 BulletForge::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
+  config.middleware.insert_before ActionDispatch::Static, "Rack::SSL"
+
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -30,7 +32,7 @@ BulletForge::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
-  
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto    => true,
@@ -43,5 +45,3 @@ BulletForge::Application.configure do
     :tls            => true
   }
 end
-
-

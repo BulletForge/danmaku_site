003321d176b70bdd9f577e6c3ee27ebfdd104ab2
Abner Qian <LargeBagel@gmail.com>
2021-07-18 22:07:14 -0700

    Changing permalink limit to 254 because 64 is too short for some already existing data in production

--- db/migrate/20210705015242_add_limit_to_permalink_columns.rb
+++ db/migrate/20210705015242_add_limit_to_permalink_columns.rb
@@ -1,6 +1,6 @@
 class AddLimitToPermalinkColumns < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :permalink, :string, :limit => 64
-    change_column :projects, :permalink, :string, :limit => 64
+    change_column :users, :permalink, :string, :limit => 254
+    change_column :projects, :permalink, :string, :limit => 254
   end
 end
